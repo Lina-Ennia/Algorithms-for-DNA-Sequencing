ac8bf3c73980b0ebf9843cede9227f4f85f79c1e
Lina Ennia <92579850+Lina-Ennia@users.noreply.github.com>
2022-10-16 12:15:12 +0100

    Update exact-matching.py

--- exact-matching.py
+++ exact-matching.py
@@ -1,11 +1,11 @@
 def exact(p, t):
-  occurrences = []
-  for i in xrange(len(t) - len(p) + 1):
-    match = True
-    for j in xrange(len(p)):
-      if t[i+j] != p[j]:
-        match = False
-        break
-    if match:
-      occurences.append(i)
-  return occurences
+    occurrences = []
+    for i in xrange(len(t) - len(p) + 1):
+        match = True
+        for j in xrange(len(p)):
+            if t[i+j] != p[j]:
+                match = False
+                break
+        if match:
+          occurences.append(i)
+    return occurences
