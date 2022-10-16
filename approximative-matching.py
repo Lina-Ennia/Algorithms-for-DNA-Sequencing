d9d7ddcb663585cccb9318e103d8e206a7df0902
Lina Ennia <92579850+Lina-Ennia@users.noreply.github.com>
2022-10-16 11:57:38 +0100

    Create approximative-matching.py

---
+++ approximative-matching.py
@@ -0,0 +1,14 @@
+def approx(p, t, maxDistance):
+    occurrences = []
+    for i in xrange(lent(t) - len(p) + 1):
+        num = 0
+        match = True
+        for j in xrange(len(p)):
+            if t[i+j] != p[j]:
+                num += 1
+                if num > maxDistance:
+                    break
+        if num <= maxDistance:
+          occurences.append(i)
+    return occurences
+      
